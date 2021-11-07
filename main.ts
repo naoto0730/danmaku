4eb8064a94e9f59b5134629df72be4268cb659ca
naoto0730 <92634097+naoto0730@users.noreply.github.com>
2021-11-07 10:30:04 +0900

    更新 pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,70 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+namespace SpriteKind {
+    export const tama = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy()
     info.changeLifeBy(-1)
     pause(100)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile2 = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . 3 1 1 3 . . . . . . 
-        . . . . . 2 1 1 1 1 2 . . . . . 
-        . . . . . 2 1 1 1 1 2 . . . . . 
-        . . . . . . 3 1 1 3 . . . . . . 
-        . . . . . . . 2 2 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, mySprite, 0, -180)
+    projectile2 = sprites.create(img`
+        ...........ccccc66666...........
+        ........ccc4444444444666........
+        ......cc444444444bb4444466......
+        .....cb4444bb4444b5b444444b.....
+        ....eb4444b5b44444b44444444b....
+        ...ebb44444b4444444444b444446...
+        ..eb6bb444444444bb444b5b444446..
+        ..e6bb5b44444444b5b444b44bb44e..
+        .e66b4b4444444444b4444444b5b44e.
+        .e6bb444444444444444444444bb44e.
+        eb66b44444bb444444444444444444be
+        eb66bb444b5b44444444bb44444444be
+        fb666b444bb444444444b5b4444444bf
+        fcb666b44444444444444bb444444bcf
+        .fbb6666b44444444444444444444bf.
+        .efbb66666bb4444444444444444bfe.
+        .86fcbb66666bbb44444444444bcc688
+        8772effcbbbbbbbbbbbbbbbbcfc22778
+        87722222cccccccccccccccc22226678
+        f866622222222222222222222276686f
+        fef866677766667777776667777fffef
+        fbff877768f86777777666776fffffbf
+        fbeffeefffeff7766688effeeeefeb6f
+        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+        f66ddfffffeeeffeffeeeeeffeedb46e
+        .c66ddd4effffffeeeeeffff4ddb46e.
+        .fc6b4dddddddddddddddddddb444ee.
+        ..ff6bb444444444444444444444ee..
+        ....ffbbbb4444444444444444ee....
+        ......ffebbbbbb44444444eee......
+        .........fffffffcccccee.........
+        ................................
+        `, SpriteKind.tama)
+    projectile2.setPosition(mySprite.x, mySprite.y)
+    projectile2.vy = -50
 })
 let projectile: Sprite = null
 let projectile2: Sprite = null
 let mySprite: Sprite = null
+let tama = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 9 9 . . . . . . . 
+    . . . . . . . 9 9 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
 tiles.setTilemap(tilemap`レベル1`)
 mySprite = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -75,31 +115,22 @@ let mySprite2 = sprites.create(img`
     `, SpriteKind.Enemy)
 mySprite2.vx = -120
 game.onUpdateInterval(100, function () {
-    for (let index = 0; index < 4; index++) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 4 4 . . . . . . . 
-            . . . . . . 4 5 5 4 . . . . . . 
-            . . . . . . 2 5 5 2 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, mySprite2, randint(-1000, 1000), randint(-1000, 1000))
-    }
-})
-game.onUpdateInterval(100, function () {
     if (mySprite2.isHittingTile(CollisionDirection.Left)) {
         mySprite2.vx = 120
     } else if (mySprite2.isHittingTile(CollisionDirection.Right)) {
         mySprite2.vx = -120
+    }
+})
+game.onUpdateInterval(300, function () {
+    for (let index = 0; index < 2; index++) {
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 50, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 0, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, -50, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 75, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, -75, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 63, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 63, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, 30, 50)
+        projectile = sprites.createProjectileFromSprite(tama, mySprite2, -30, 50)
     }
 })
